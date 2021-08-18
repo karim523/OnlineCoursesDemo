0c11acd093d1bb9f3d0fd5109dfd14da99bed819
rafamarquesrmb <rafa.rmb@gmail.com>
2021-08-18 18:49:47 -0300

    Modelling Career

--- ContentContext/Career.cs
+++ ContentContext/Career.cs
@@ -1,8 +1,17 @@
 using System;
+using System.Collections.Generic;
+
 namespace SimpleObjects.ContentContext
 {
     public class Career : Content
     {
+        public Career()
+        {
+            Items = new List<CareerItem>();
+        }
+        public IList<CareerItem> Items { get; set; }
+        public int CoursesCount => Items.Count;
         
     }
+    
 }
---
+++ ContentContext/CareerItem.cs
@@ -0,0 +1,10 @@
+namespace SimpleObjects.ContentContext
+{
+    public class CareerItem
+    {
+        public int Order {get; set;}
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Course Course { get; set; }
+    }
+}
