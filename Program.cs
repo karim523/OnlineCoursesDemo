8e038902f21858fe3fd7a7d81cf0ba9cc425219c
rafamarquesrmb <rafa.rmb@gmail.com>
2021-08-20 12:22:55 -0300

    Seeding

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using SimpleObjects.ContentContext;
 
 namespace SimpleObjects
@@ -13,11 +14,38 @@ namespace SimpleObjects
             articles.Add(new Article("Artigo sobre C#","csharp"));
             articles.Add(new Article("Artigo sobre .NET","net"));
             
-            foreach(var article in articles)
+            // foreach(var article in articles)
+            // {
+            //     Console.WriteLine(article.Id);
+            //     Console.WriteLine(article.Title);
+            //     Console.WriteLine(article.Url);
+            // }
+
+            var courses = new List<Course>();
+            var courseOOP = new Course("Fundamnetos OOP", "fundamentos-oop");
+            var courseCsharp = new Course("Fundamnetos C#", "fundamentos-csharp");
+            var courseAspNet = new Course("Fundamnetos APS.NET", "fundamentos-asp-net");
+            courses.Add(courseOOP);
+            courses.Add(courseCsharp);
+            courses.Add(courseAspNet);
+
+            var careers = new List<Career>();
+            var careerDotnet = new Career("Especialista .NET","especialista-dotnet");
+            var careerItem1 = new CareerItem(1,"Comece por Aqui", "", null);
+            var careerItem2 = new CareerItem(2,"Aprenda OOP", "", null);
+            var careerItem3 = new CareerItem(3,"Aprenda .NET", "", null);
+            careerDotnet.Items.Add(careerItem2);
+            careerDotnet.Items.Add(careerItem3);
+            careerDotnet.Items.Add(careerItem1);
+            careers.Add(careerDotnet);
+
+            foreach(var career in careers)
             {
-                Console.WriteLine(article.Id);
-                Console.WriteLine(article.Title);
-                Console.WriteLine(article.Url);
+                Console.WriteLine(career.Title);
+                foreach(var item in career.Items.OrderBy(x => x.Order))
+                {
+                    Console.WriteLine($"{item.Order} - {item.Title}");
+                }
             }
         }
     }
