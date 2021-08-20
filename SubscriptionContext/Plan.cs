942650d8326d5bb5947eb64997052233541f7e3b
rafamarquesrmb <rafa.rmb@gmail.com>
2021-08-20 13:51:14 -0300

    Subscription rules

---
+++ SubscriptionContext/Plan.cs
@@ -0,0 +1,11 @@
+using SimpleObjects.SharedContext;
+
+namespace SimpleObjects.SubscriptionContext
+{
+    public class Plan : Base
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        
+    }
+}
--- SubscriptionContext/Student.cs
+++ SubscriptionContext/Student.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using SimpleObjects.NotificationContext;
 using SimpleObjects.SharedContext;
 
 namespace SimpleObjects.SubscriptionContext
@@ -7,5 +10,16 @@ namespace SimpleObjects.SubscriptionContext
         public User User { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public IList<Subscription> Subscriptions { get; set; }
+        public bool IsPremium => Subscriptions.Any(x => !x.IsInactive);
+        public void CreateSubscription(Subscription subscription)
+        {
+            if(IsPremium)
+            {
+                AddNotification(new Notification("Premiun","This Students already have an Active Subscription"));
+                return;
+            }
+            Subscriptions.Add(subscription);
+        }
     }
 }
